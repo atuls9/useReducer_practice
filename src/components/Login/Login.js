3532bace6347b9bfab447cf7b0c2b06a04d268d9
atuls9 <atul.sooryawanshi@gmail.com>
2023-04-09 15:18:40 +0530

    created a reusable Input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import Reactt, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
